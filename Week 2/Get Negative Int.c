2f53052b3575b656a5e0a5daddda6bd64ae827c0
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-07 17:05:24 -0700

    Create Get Negative Int.c

---
+++ Week 2/Get Negative Int.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_negative_int(void);
+
+int main(void)
+{
+    int i = get_negative_int();
+    printf("%i\n", i);
+}
+
+int get_negative_int(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Negative integer: ");
+        printf("n is %i\n", n);
+    }
+    while (n < 0);
+    return n;
+}
