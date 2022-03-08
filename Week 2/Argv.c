9bdfd4d954317a47714f1727c29d0b6a0611cad9
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-07 20:23:25 -0700

    Create Argv.c

---
+++ Week 2/Argv.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        printf("hello, %s", argv[1]);
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
+    
+}
