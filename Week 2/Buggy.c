947276ddecbba5b0f2d4b43fb35591c98990971d
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-07 17:05:22 -0700

    Create Buggy.c

---
+++ Week 2/Buggy.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // for (int i = 0; i <= 3; i++)
+    // {
+    //     printf("#\n");
+    // }
+  
+    for (int i = 0; i < 3; i++)
+    {
+        printf("#\n");
+    }
+
+
+}
