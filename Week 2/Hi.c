251e9bb3cfd11bc80b0e39d1db69dac263407951
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-07 20:23:42 -0700

    Create Hi.c

---
+++ Week 2/Hi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
+
+    // printf("%c%c%c\n", c1, c2, c3);
+
+    string s = "HI!";
+    string t = "BYE!";
+
+    printf("%s\n",s);
+    printf("%s\n",t);
+}
