86a1cac0d1cf637d2b98dc81547c2ffb89c3c96a
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-07 20:23:33 -0700

    Create Scores.c

---
+++ Week 2/Scores.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int n = get_int("How many scores? ")
+    // double average; 
+
+    // for (i = 0; i<n; i++)
+    // {
+    //     int score[i] = get_int("Score: ");
+
+    //     average+=i;
+
+    // }
+    
+    // average=/n;
+
+    int n = get_int("How many scores? ")
+
+    int scores[n];
+
+    for (i = 0, i < n; i++)
+    {
+        scores[i] = get_int("Score: ")'';
+        int scoresum = scores[i]++;
+    }
+
+    float scoreaverage = scoresum/(float) n;
+
+
+
+}
