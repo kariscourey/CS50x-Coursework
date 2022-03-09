caf9376da72393d4d43f6c6fb9de61db9610dd22
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-09 12:52:40 -0700

    Update Readability.c

--- Week 2/Readability.c
+++ Week 2/Readability.c
@@ -1,16 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+int grade_index(int letters, int words, int sentences);
 
 int main(void)
 {
     string text = get_string("What's your text, friend?\n");
     printf("Text: %s\n", text);
 
-    int length = count_letters(text);
-    printf("%i letters\n",length);
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+    int grade = grade_index(letters, words, sentences);
+
+    if (grade < 1)
+    {
+        printf("Before Grade 1");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+");
+    }
+    else if (grade >= 1 and grade < 16)
+    {
+        printf("Grade %i", grade);
+    }
 
 }
 
@@ -21,7 +40,7 @@ int count_letters(string text)
 
     while (i < n)
     {
-        if ((text[i]) != 20)
+        if (text[i] != 32)
         {
             i++;
         }
@@ -31,4 +50,52 @@ int count_letters(string text)
         }
     }
     return i;
+}
+
+int count_words(string text)
+{
+    int i = 0;
+    int n = strlen(text);
+
+    while (i < n)
+    {
+        if (text[i]) == 32)
+        {
+            i++;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return i;
+}
+
+int count_sentences(string text)
+{
+    int i = 0;
+    int n = strlen(text);
+
+    while (i < n)
+    {
+        if (text[i] == 46 or text[i] == 44 or text[i] == 63)
+        {
+            i++;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return i;
+}
+
+int grade_index(int letters, int words, int sentences)
+{
+    float L = letters / words * 100.0;
+    float S = sentences / words * 100.0;
+    
+    int index = round(0.0588*L - 0.296*S - 15.8);
+
+    return index;
 }
