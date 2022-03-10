7c71ea19aea13d63514d14ae5e798406be16ec9d
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-09 21:12:46 -0700

    Create caesar.c

---
+++ Week 2/caesar.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+
+//check if one arg (argc == 1), else output error, return 1
+
+//check if arg is int, else output "Usage: ./caesar key", return 1
+
+//check if arg is positive
+
+//ci = (pi + k) % 26, k = input
+
+//for each LETTER in input, add k (maintain case)
+
+    //if LETTER + k != bet 65 and 90 (uppercase) or bet 97 and 122 (lowercase)
+    //(LETTER + k) - 90 or (LETTER + k) - 122
+
+//output "plaintext:  "
+
+//prompt user for string
+
+//output "ciphertext: CIPHERTEXT"
+
+//output newline
+
+//return 0
