f7fcd0980524884107857a2864f5f433f0eba18b
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-09 21:24:00 -0700

    Update Readability-better.c

--- Week 2/Readability-better.c
+++ Week 2/Readability-better.c
@@ -3,69 +3,72 @@
 #include <string.h>
 #include <math.h>
 
-int count_letters(string text);
-int count_words(string text);
-int count_sentences(string text);
+//prototypes
 int grade_index(int letters, int words, int sentences);
+void grade_level(int index);
 
 int main(void)
 {
-    string text = get_string("What's your text, friend?\n");
-    printf("Text: %s\n", text);
+    //prompt user for input
+    string input = get_string("What's your text, friend?\n");
 
-    int letters = count(text);
-    int words = count(text);
-    int sentences = count(text);
-    int grade = grade_index(letters, words, sentences);
-
-    if (grade < 1)
-    {
-        printf("Before Grade 1");
-    }
-    else if (grade >= 16)
-    {
-        printf("Grade 16+");
-    }
-    else if (grade >= 1 and grade < 16)
-    {
-        printf("Grade %i", grade);
-    }
-
-}
-
-int * count(string text)
-{
+    //define variable for grade index
     int i = 0;
-    int j = 0;
-    int k = 0;
-    int n = strlen(text);
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+    int n = strlen(input);
 
     while (i < n)
     {
-        if (text[i]) != 32)
+        if ((input[i] >= 65 && input[i] <= 90) || (input[i] >= 97 && input[i] <= 122))
         {
-            i++;
+            letters++;
         }
-        else if ((text[i] == 32))
+        else if (input[i] == 32)
         {
-            j++;
+            words++;
         }
-        else if (text[i] == 46 or text[i] == 44 or text[i] == 63))
+        else if (input[i] == 46 || input[i] == 33 || input[i] == 63)
         {
-            k++;
+            sentences++;
         }
     }
 
-    int values = [i,j,k];
-    return values;
+    //determine grade index
+    int index = grade_index(letters, words, sentences);
+
+    //determine grade level
+    grade_level(index);
 }
 
+
+//returns grade index
 int grade_index(int letters, int words, int sentences)
 {
-    float L = letters / words * 100.0;
-    float S = sentences / words * 100.0;
-    
-    int index = round(0.0588*L - 0.296*S - 15.8);
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    //coleman lieu algorithm
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
 
     return index;
+}
+
+
+//returns grade level
+void grade_level(int index)
+{
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (index >= 1 && index < 16)
+    {
+        printf("Grade %i\n", index);
+    }
 }
