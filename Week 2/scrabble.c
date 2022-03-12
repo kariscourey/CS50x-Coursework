43cb6c450bd6ca2dd0fb82f4a49c569cfd03661f
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-11 20:37:57 -0700

    Update scrabble.c

--- Week 2/scrabble.c
+++ Week 2/scrabble.c
@@ -3,34 +3,34 @@
 #include <stdio.h>
 #include <string.h>
 
-// Points assigned to each letter of the alphabet
+// points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
 
 int main(void)
 {
-    //initialize Player #
+    //initialize player #
     string player1 = "Player 1";
     string player2 = "Player 2";
     string winner;
 
-    // Get input words from both players
+    // get input words from both players
     string word1 = get_string("%s: ", player1);
     string word2 = get_string("%s: ", player2);
 
-    // Score both words
+    // score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // Print the winner
-    if (score1 = score2)
+    // print winner
+    if (score1 == score2)
     {
-        printf("Tie!");
+        printf("Tie!\n");
     }
     else
     {
-        if (score 1 > score 2)
+        if (score1 > score2)
         {
             winner = player1;
         }
@@ -38,31 +38,35 @@ int main(void)
         {
             winner = player2;
         }
-        printf("%s wins!", player2);
+        printf("%s wins!\n", winner);
     }
 }
 
 int compute_score(string word)
 {
-    // Compute and return score for string
+    // compute and return score for string
 
     int score = 0;
     int index;
     int low;
     int high;
+    bool letter = false;
 
     for (int i = 0, n = strlen(word); i < n; i++)
     {
+        //check if letter
         if (isupper(word[i]))
         {
             low = 65;
             high = 90;
+            letter = true;
         }
 
         else if (islower(word[i]))
         {
             low = 97;
             high = 122;
+            letter = true;
         }
 
         else
@@ -70,9 +74,12 @@ int compute_score(string word)
             continue;
         }
 
-        index = word[i] - low;
-        score = score + POINTS[index];
-
-
+        //if letter, add to score
+        if (letter == true)
+        {
+            index = word[i] - low;
+            score = score + POINTS[index];
+        }
     }
+    return score;
 }
