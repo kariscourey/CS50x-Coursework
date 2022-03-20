b485315f98d74c9f36506c791f8bb58be6459f75
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-20 17:58:04 -0600

    Create copy.c

---
+++ Week 4/copy.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // string s = get_string("s: ");
+
+    // string s = t;
+
+    char *s = get_string("s: ");
+    char *t = malloc(strlen(s) + 1);
+
+    if (t == NULL)
+    {
+        return 1;
+    }
+
+    strcpy(t, s);
+
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
+}
