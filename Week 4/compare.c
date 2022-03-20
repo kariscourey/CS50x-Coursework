cb7f75fa0fcf5a53e0807914399c7af440d391ab
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-20 17:58:27 -0600

    Create compare.c

---
+++ Week 4/compare.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    // int i = get_int("i: ");
+    // int j = get_int ("j: ");
+
+    // string s = get_string("s: ");
+    // string t = get_string("t: ");
+
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+
+    // if (i == j)
+
+    // if (s == t)
+    // if (strcmp(s, t) == 0)
+    // {
+    //     printf("Same\n");
+    // }
+    // else
+    // {
+    //     printf("Different\n");
+    // }
+
+    printf("%p\n", s);
+    printf("%p\n", t);
+}
