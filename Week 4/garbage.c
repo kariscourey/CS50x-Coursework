14a7cf88a9593c5e082e49883d0f9a4f351e6667
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-20 17:58:20 -0600

    Create garbage.c

---
+++ Week 4/garbage.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int scores[3];
+    
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%i\n", scores[i]); //didn't initialize scores array
+    }
+}
