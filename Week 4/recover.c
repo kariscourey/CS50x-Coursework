b049368d092de8868a66debd850ba28c14e6b26e
Karis Courey <94191734+kariscourey@users.noreply.github.com>
2022-03-23 16:29:37 -0600

    Update recover.c

--- Week 4/recover.c
+++ Week 4/recover.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+
+typedef uint8_t  BYTE;
 
 int main(int argc, char *argv[])
 {
+    errno = 0;
+
+    //check if one arg
+    if (argc != 2)
+    {
+        printf("Usage: ./recover key\n");
+        return 1;
+    }
+
+    //set pointer to file
     FILE *file = fopen(argv[1], "r");
-    typedef uint8_t BYTE;
+
+    if (file == NULL)
+    {
+        //return error if null
+        printf("Error: ./recover %d\n", errno);
+        return 1;
+    }
+
 
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
-
-
     }
 }
