478fe9f9313cb4054855aa82e3cec28bad7995e5
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-03-22 21:03:08 -0600

    Create recover.c

---
+++ Week 4/recover.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *file = fopen(argv[1], "r");
+    typedef uint8_t BYTE;
+
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    {
+
+
+    }
+}
