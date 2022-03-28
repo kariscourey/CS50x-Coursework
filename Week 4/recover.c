548a3a78d1231beff099086b29c33142a6539d7e
Karis Courey <94191734+kariscourey@users.noreply.github.com>
2022-03-27 20:56:04 -0600

    Update recover.c

--- Week 4/recover.c
+++ Week 4/recover.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <errno.h>
 
-typedef uint8_t  BYTE;
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    errno = 0;
-
     //check if one arg
     if (argc != 2)
     {
@@ -17,17 +14,58 @@ int main(int argc, char *argv[])
     }
 
     //set pointer to file
-    FILE *file = fopen(argv[1], "r");
+    FILE *input = fopen(argv[1], "r");
 
-    if (file == NULL)
+    if (input == NULL)
     {
         //return error if null
-        printf("Error: ./recover %d\n", errno);
+        printf("Error: ./recover pointer\n");
         return 1;
     }
 
+    int qty = 0;
+    FILE *output = NULL;
+    char *filename = malloc(8 * sizeof(char));
+    BYTE buffer[512];
 
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    while (fread(buffer, 1, 512, input) == 512)
     {
+        //look for 0xff, 0xd8, 0xff, 0xe_ (indicates start of jpeg)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            //if not first jpeg
+            if (qty != 0)
+            {
+                //close file
+                fclose(output);
+            }
+
+            //print file name to string, filename
+            sprintf(filename, "%03i.jpg", qty);
+
+            //open filename
+            output = fopen(filename, "w");
+
+            //increment qty
+            qty++;
+        }
+
+        //if already found jpeg
+        if (output != NULL)
+        {
+            //write remaining bytes to file
+            fwrite(buffer, 1, 512, output);
+        }
     }
+
+    //free memory
+    free(filename);
+
+    //close jpeg file
+    fclose(output);
+
+    //close arg file
+    fclose(input);
+
+    return 0;
 }
