161a67bf904b4cfd4e805087b22476bdb2ac170d
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-04-12 21:31:28 -0600

    Create mario.py

---
+++ Week 6/mario.py
@@ -0,0 +1,34 @@
+# from cs50 import get_int
+
+# n = get_int("Height: ")
+
+# for in range(n):
+#     print("#")
+
+# def main():
+#     height = get_height()
+#     for i in range(height):
+#         print("#")
+
+# def get_height():
+#     while True:
+#         # n = get_int("Height: ")
+#         try:
+#             n = int(input("Height: "))
+#             if n > 0:
+#                 break
+#         except ValueError:
+#             print("Not an int")
+#     return n 
+
+# main()
+
+# for i in range(4):
+#     print("?", end="")
+# print()
+
+for i in range(3):
+    # for j in range(3):
+    #     print("#", end = "")
+    # print()
+    print("#" * 3)
