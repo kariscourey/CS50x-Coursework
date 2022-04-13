f7267278cad2deaf254153cb49a82e1a0999ae38
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-04-12 21:31:12 -0600

    Create uppercase.py

---
+++ Week 6/uppercase.py
@@ -0,0 +1,11 @@
+from cs50 import get_string
+
+before = get_string("Before: ")
+# after = before.upper()
+
+# print("After: {after}")
+print(f"After: {before.upper()}")
+
+# for c in before:
+#     print(c.upper(),end="")
+# print()
