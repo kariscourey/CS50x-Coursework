1ac04eb401b4c201aad3b7a8700e83d68fd91544
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-04-13 21:20:54 -0600

    Create mario-more-pset.py

---
+++ Week 6/mario-more-pset.py
@@ -0,0 +1,37 @@
+class RangeError(Exception):
+    # define error if value out of range
+    pass
+
+
+def main():
+    # populate height
+    height = get_height()
+
+    # print pyramid
+    for i in range(1, height + 1):
+        print(" " * (height - i) + "#" * i + "  " + "#" * i)
+
+
+def get_height():
+    while True:
+        try:
+            # get user input
+            height = int(input("Height?: "))
+
+            # evaluate input against acceptable range
+            if height <= 0 or height >= 9:
+                raise RangeError
+
+        # check if not an int
+        except ValueError:
+            print("Not an int!")
+
+        # check if in range
+        except RangeError:
+            print("Not within range!")
+
+        else:
+            return height
+
+
+main()
