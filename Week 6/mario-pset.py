6a6358817b188317f11a89db5ccc0bf480bbe32f
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-04-13 20:43:48 -0600

    Update mario-pset.py

--- Week 6/mario-pset.py
+++ Week 6/mario-pset.py
@@ -1,12 +1,19 @@
 from sys import exit
 
+
+class RangeError(Exception):
+    # define error if value out of range
+    pass
+
+
 def main():
     # populate height
     height = get_height()
 
-    #print # for each item in height
-    for i in range(1,height + 1):
+    # print # for each item in height
+    for i in range(1, height + 1):
         print(" " * (height - i) + "#" * i)
+
 
 def get_height():
     while True:
@@ -14,14 +21,20 @@ def get_height():
             # get user input
             height = int(input("Height?: "))
 
+            # evaluate input against acceptable range
+            if height <= 0 or height >= 9:
+                raise RangeError
+
         # check if not an int
         except ValueError:
             print("Not an int!")
 
-        except height < 0 or height > 9:
+        # check if in range
+        except RangeError:
             print("Not within range!")
 
         else:
             return height
+
 
 main()
