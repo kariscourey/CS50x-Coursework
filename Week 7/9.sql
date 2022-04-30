5776d23ee26d054e57ffa1d362daaca6261147c9
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-04-29 23:20:43 -0600

    Create 9.sql

---
+++ Week 7/9.sql
@@ -0,0 +1,9 @@
+/*List names of stars in 2004 movies, ordered by birth year*/
+SELECT people.name
+FROM people
+JOIN stars
+ON people.id = stars.person_id
+JOIN movies
+ON stars.movie_id = movies.id
+WHERE movies.year = 2004
+ORDER BY people.birth;
