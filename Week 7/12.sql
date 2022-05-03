f98d5c2c6b38fc858f204bf7dc2924e27b65c732
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-05-03 16:03:58 -0600

    Create 12.sql

---
+++ Week 7/12.sql
@@ -0,0 +1,12 @@
+/*List titles of five highest ratest movies with Chadwick Boseman*/
+SELECT movies.title
+FROM movies
+JOIN stars
+ON movies.id = stars.movie_id
+JOIN people
+ON stars.person_id = people.id
+JOIN ratings
+ON movies.id = ratings.movie_id
+WHERE people.name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC
+LIMIT 5;
