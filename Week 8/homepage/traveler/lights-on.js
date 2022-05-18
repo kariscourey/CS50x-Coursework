cd710d2e9af3353115f1d187c93d36f906dcbeb2
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-05-18 17:01:45 -0600

    Create lights-on.js

---
+++ Week 8/homepage/traveler/lights-on.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#light').click(function() {
+        $('body').css('background-color', '#a55f0d');
+        $('body').css('color', '#241e20');
+    })
+    $('#better').show();
+});
