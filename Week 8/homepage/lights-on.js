761b237090a5d085172d73a393e37b7c40a17663
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-05-18 17:02:03 -0600

    Organize directory

--- Week 8/homepage/lights-on.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#light').click(function() {
-        $('body').css('background-color', 'white');
-        $('body').css('color', 'black');
-    })
-    $('#better').show();
-});
