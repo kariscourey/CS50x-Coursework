db2fd57d4760cdd93dd5d00867191c79643095b6
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-05-19 16:35:02 -0600

    Delete tohelp.js

--- Week 8/homepage/traveler/tohelp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.tohelp').click(function() {
-        window.location.href='help.html';
-    });
-    $('.tohelplight').click(function() {
-        window.location.href='help.html';
-        $('link[id="theme"]').attr('href', "styles-light.css");
-    });
-});
