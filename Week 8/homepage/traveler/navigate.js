c034ae6d1e11a98a8a6cb55a797032e993cab812
kariscourey <94191734+kariscourey@users.noreply.github.com>
2022-05-19 16:35:15 -0600

    Create navigate.js

---
+++ Week 8/homepage/traveler/navigate.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.home').click(function() {
+        window.location.href='home.html';
+    });
+    $('.about').click(function() {
+        window.location.href='about.html';
+    });
+    $('.github').click(function() {
+        window.location.href='github.html';
+    });
+    $('.linkedin').click(function() {
+        window.location.href='linkedin.html';
+    });
+});
