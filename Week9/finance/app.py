a39ae8e5017a7dd19a208778edd84008cbf8ae57
Karis Courey <94191734+kariscourey@users.noreply.github.com>
2022-07-05 17:11:04 -0600

    Update app.py

--- Week9/finance/app.py
+++ Week9/finance/app.py
@@ -30,6 +30,7 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
